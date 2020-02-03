d3cead2d4cdbaf3eb5cb44ce50a279fc92f630a3
franznastor <franznastor@gmail.com>
2020-02-02 20:28:04 -0600

    Created "answers" to contain... the answers.

--- Questions.py
+++ Questions.py
@@ -3,32 +3,43 @@ import json
 questions_string = '''
 
 {
-    "questions":
+    "Questions":
     [
         {
             "category" : "True/False",
             "ask_question" : "Insert a true or false question",
-            "answer1" : true,
-            "answer2" : false
+            "answers" :
+            {
+                "answer1" : true,
+                "answer2" : false
+            }
         },
+        
         {
             "category" : "Multiple Choice",
             "ask_question" : "Insert a multiple choice question",
-            "answer1" : "A",
-            "answer2" : "B",
-            "answer3" : "C",
-            "answer4" : "D",
+            "answers": 
+            {
+                "answer1" : "A",
+                "answer2" : "B",
+                "answer3" : "C",
+                "answer4" : "D"
+            }
         },
+        
         {
             "category" : "Matching",
             "ask_question" : "Insert matching question",
-            "answer1" : "Insert matching description",
-            "answer2" : "Insert matching description",
-            "answer3" : "Insert matching description",
-            "answer4" : "Insert matching description",
+            "answers" : 
+            
+            {
+                "answer1" : "Insert matching description",
+                "answer2" : "Insert matching description",
+                "answer3" : "Insert matching description",
+                "answer4" : "Insert matching description"
+            }
         }
     ]
 }
-
 
 '''
