330d4845c579d76deb06a294fcdf041c9358ec9a
Sam Alexander <salexander@luc.edu>
2020-02-07 13:09:20 -0600

    Testing file. Commit #2 (was from Wednesday, will push more commits from updated file today).
    Signed-off-by: Sam Alexander <salexander@luc.edu>

--- test.py
+++ test.py
@@ -1,24 +1,30 @@
-Name = input("Enter your username: ")
-print("Hello", Name + "." + " Welcome to Quiz-Click!")
 
-score = 0
-for i in question:
-    print()
-    print(i)
-    checkpoint1 = input("Do you want to skip this question (Y/N): ")
-    if checkpoint1 == "Y":
-        continue
-    ans = input("Enter the answer A, B, C, or D: ")
-    if ans == question[i]:
-        print("Correct answer, you got 1 point!")
-        score = score + 1
-        print("Your current score is: ", score)
-    else:
-        print("Wrong answer, you lost 1 point.")
-        score = score - 1
-        print("Your current score is: ", score)
-    checkpoint2 = input("Do you want to quit (Y/N): ")
-    if checkpoint2 == "Y":
-        break
+def main():
+    Name = input("Enter your username: ")
+    print("Hello", Name + "." + " Welcome to Quiz-Click!")
 
-print("Your final score is: ", score)
+
+def run():
+    score = 0
+    for i in question:
+        print()
+        print(i)
+        checkpoint1 = input("Do you want to skip this question (Y/N): ")
+        if checkpoint1 == "Y":
+            continue
+        ans = input("Enter the answer A, B, C, or D: ")
+        if ans == question[i]:
+            print("Correct answer, you got 1 point!")
+            score = score + 1
+            print("Your current score is: ", score)
+        else:
+            print("Wrong answer, you lost 1 point.")
+            score = score - 1
+            print("Your current score is: ", score)
+        checkpoint2 = input("Do you want to quit (Y/N): ")
+        if checkpoint2 == "Y":
+            break
+    print('Your final score is: ', score)
+
+if __Name__ == "__main__":
+    main()
