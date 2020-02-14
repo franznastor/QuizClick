5e5025423efe6aea7b441b1e0ab2b6c0d7161019
Sam Alexander <salexander@luc.edu>
2020-02-14 09:42:16 -0600

    Still working on this file
    Signed-off-by: Sam Alexander <salexander@luc.edu>

--- test.py
+++ test.py
@@ -1,47 +1,88 @@
 import json
-import random
 
 
-def get_questions():
-    questions = []
-    with open("data.txt", mode="r", encoding="utf-8") as my_file:
-        reader = json.reader(my_file)
-        for row in reader:
-            questions.append(row)
-    return questions
+def get_input():
+    with open('data.txt') as file:
+        data = json.load(file)
+
+    # user input
+    for question in data["question"]:
+        print(question["'category"])
+        choose = input("Please choose your category: ")
+        if choose == "True/False":
+            print(data["ask_question"])
+            insert = input("Insert a true of false question: ")
+            if insert == "answers":
+                print("answer1")
+                print("answer2")
+                ans = input("Select your answer: ")
+                if ans == "answer1" is True or ans == "answer2" is True:
+                    print("Correct! You receive a point.")
+                else:
+                    print("Sorry, your answer is wrong! You do not receive a point.")
+                return get_input()
+        if choose == "Multiple Choice":
+            print(data["ask_question"])
+            insert1 = input("Insert a multiple choice question: ")
+            if insert1 == "answers":
+                print("answer1")
+                print("answer2")
+                print("answer3")
+                print("answer4")
+                ans1 = input("Select your answer: ")
+                if ans1 == "answer1" or ans1 == "answer2" or ans1 == "answer3" or ans1 == "answer4" is True:
+                    print("Correct! You receive a point.")
+                else:
+                    print("Sorry, your answer is wrong! You do not receive a point.")
+                return get_input()
+        if choose == "Matching":
+            print(data["ask_question"])
+            insert2 = input("Insert a matching question: ")
+            if insert2 == "answers":
+                print("answer1")
+                print("answer2")
+                print("answer3")
+                print("answer4")
+                ans2 = input("Select your answer: ")
+                if ans2 == "answer1" or ans2 == "answer2" or ans2 == "answer3" or ans2 == "answer4" is True:
+                    print("Correct! You receive a point.")
+                else:
+                    print("Sorry, your answer is wrong! You do not receive a point.")
+                return get_input()
 
 
-def ask_questions(question, score):
-    print(question[0])
-    for multi in ask_questions[1:-1]:
-        print("{0:>5}{1}".format("", multi))
-    answer = input("Please select an answer: ")
-    print()
-    if answer == question[-1]:
-        print("Correct!")
-        score += 1
-    else:
-        print("Incorrect! The correct answer was {0}.".format(question[-1]))
-    print()
-    return score
-
-
-def main():
-    Name = input("Enter your username: ")
-    print("Hello", Name + "." + " Welcome to Quiz-Click!")
-    questions = get_questions()
+def lets_begin():
+    # still working on this part
+    name = input("Enter your username: ")
+    print("Hello, {0}! Welcome to Quiz Click!".format(name))
+    print("Lets begin!\n")
+    play = get_input()
     score = 0
-    print("Welcome to Quiz-Click!")
-    print("==========================")
     print()
     print()
-    number = int(input("There are {0} questions - how many would you like in the quiz?"))
     for next_question in range(number):
-        question = random.choice(questions)
-        score = ask_questions(question, score)
-        questions.remove(question)
+        question = random.choice(play)
+
     print("Your final score was {0} out of {1}.".format(score, number))
 
+    with open('answers.json', 'w', encoding='utf-8') as new_data:
+        json.dump(new_data, indent=3)
 
-if __name__ == "__main__":
-    main()
+    # data = json.loads(open('quiz_click.json').read())
+    # '''The .loads() method converts a JSON string to a Python dictionary'''
+    #
+    # # print(data)
+    # '''Output is a Python dictionary'''
+    #
+    # # print(type(data))
+    # '''Confirms that data is a dict object'''
+    #
+    # # for question in data['questions']:
+    # #    print(question['category'])
+    # '''Output is all the categories'''
+    #
+    # new_data = json.dumps(data, indent=3)
+    # '''The .dumps() method converts a Python object into a JSON string'''
+    #
+    # with open('quiz_click.json', 'w', encoding='utf-8') as file:
+    #     json.dump(file)
